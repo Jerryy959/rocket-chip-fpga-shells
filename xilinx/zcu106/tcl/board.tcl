4d0eddad5f8f209e968a3c6f3b8ccdf61fdebf8a
Jerryy959 <1376723538@qq.com>
2024-10-24 17:16:10 +0000

    add zcu102 support for fpga-shell

---
+++ xilinx/zcu106/tcl/board.tcl
@@ -0,0 +1,6 @@
+# See LICENSE for license details.
+set name {zcu106}
+set part_fpga {xczu7ev-ffvc1156-2-e}
+# Board: xilinx.com:vcu118:part0:2.0
+# However, if we set this we cannot control PCIe locations
+set part_board {}
